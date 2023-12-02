c75c7eedba28fb3067f33a55f234c670df8ffc98
Samms206 <sh2069936@gmail.com>
2023-12-02 23:53:36 +0700

    firts commit

---
+++ src/koneksi.java
@@ -0,0 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author macbookpro
+ */
+public class koneksi {
+    Connection koneksi;
+    public static Connection Koneksi() {
+        try {
+            Connection koneksi = (Connection)DriverManager.getConnection("jdbc:mysql://localhost/db_test", "root", "");
+            return koneksi;
+        } catch(SQLException e) {
+            JOptionPane.showMessageDialog(null, "lost connection : "+e);
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        Koneksi();
+    }
+}
